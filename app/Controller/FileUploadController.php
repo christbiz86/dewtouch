160ae81ff1868ee45ab80b101163505a8742d989
Christian K <christ.biz@gmail.com>
2020-03-13 00:41:44 +0700

    add validation

--- app/Controller/FileUploadController.php
+++ app/Controller/FileUploadController.php
@@ -1,10 +1,30 @@
 <?php
-
+    
 class FileUploadController extends AppController {
-	public function index() {
-		$this->set('title', __('File Upload Answer'));
-
-		$file_uploads = $this->FileUpload->find('all');
-		$this->set(compact('file_uploads'));
-	}
+        
+    public function index() {
+        if ($this->request->is('post')) {
+            if(!empty($this->data['FileUpload']['file']['name'])){
+                $checkFileType = $this->FileUpload->checkFileType($this->data['FileUpload']['file']);
+                if(!$checkFileType){
+                    $error = "Only support csv files !!!";
+                } else {
+                    $error = "";
+                }
+                
+                $file = fopen(WWW_ROOT . 'files/' . $this->data['FileUpload']['file']['name'], "r");
+                while (($getData = fgetcsv($file, 10000, ",")) !== FALSE){
+                    var_dump($getData);
+                }
+                fclose($file);
+                exit();
+           }    
+        } else {
+            $error = "";
+        }
+        $this->set('title', __('File Upload Answer'));
+        $file_uploads = $this->FileUpload->find('all');
+        $this->set(compact('file_uploads','error'));
+    }
+    
 }
--- app/Model/FileUpload.php
+++ app/Model/FileUpload.php
@@ -2,4 +2,23 @@
 
 class FileUpload extends AppModel {
 
+    private $allowed_extension = array(
+//        "doc",
+//        "docx",
+//        "pdf",
+//        "xls",
+//        "xlsx",
+        "csv"
+    );
+    
+    public function checkFileType($file){
+        $file_extension = pathinfo($file['name'], PATHINFO_EXTENSION);
+        if(in_array($file_extension,$this->allowed_extension)) {
+            move_uploaded_file($file['tmp_name'], WWW_ROOT . 'files/' . $file['name']);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
 }
